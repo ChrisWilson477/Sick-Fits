f9e5530217cc32c3df968be3ca8c614836f728f0
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-07-08 16:55:09 -0600

    Sick Fits

---
+++ components/Pagination.js
@@ -0,0 +1,42 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import Link from 'next/link';
+import PaginationStyles from './styles/PaginationStyles';
+import ErrorMessage from './ErrorMessage';
+import { perPage } from '../config';
+
+export const PAGINATION_QUERY = gql`
+  query {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { loading, data, error } = useQuery(PAGINATION_QUERY);
+  if (loading) return 'Loading...';
+  if (error) return <ErrorMessage error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>
+          Sick Fits | Page {page} of {pageCount}
+        </title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page <= 1}>&#8592; Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
+      <p>{count} Items Total</p>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>&#8594; Next</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
